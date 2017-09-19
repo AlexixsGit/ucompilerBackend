e82812b01ddf64be11c89a7b4df791a812cb6ddc
Edward Alexis Ortiz <e.du1991@hotmail.com>
2017-09-18 20:51:54 -0500

    First commit, In this commit is the main structure of a spring boot project with an index page

---
+++ src/main/java/com/itm/ucompiler/UcompilerApplication.java
@@ -0,0 +1,12 @@
+package com.itm.ucompiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcompilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcompilerApplication.class, args);
+	}
+}
